4852dd7e630442c865448c9ef56985722c8a3363
Mario <aubert.mario@gmail.com>
2020-11-22 13:13:15 +0100

    Synchronisation en vue de la coupe 2021.

--- Debug/Core/Src/F_Odemetry.d
+++ Debug/Core/Src/F_Odemetry.d
@@ -61,9 +61,9 @@ Core/Src/F_Odemetry.o: ../Core/Src/F_Odemetry.c ../Core/Inc/F_Odometry.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_dma.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_exti.h \
  ../Core/Inc/gpio.h \
- /home/guillaume/STM32CubeIDE/workspace_1.1.0/CDFR_2020/Drivers/API_VL53L1_ULD/core/VL53L1X_api.h \
- /home/guillaume/STM32CubeIDE/workspace_1.1.0/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_platform.h \
- /home/guillaume/STM32CubeIDE/workspace_1.1.0/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_types.h \
+ /home/mario/Documents/ARM_DEV/CDFR_2020/Drivers/API_VL53L1_ULD/core/VL53L1X_api.h \
+ /home/mario/Documents/ARM_DEV/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_platform.h \
+ /home/mario/Documents/ARM_DEV/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_types.h \
  ../Core/Inc/F_VL53L1X.h ../Core/Inc/i2c.h ../Core/Inc/F_LCD.h \
  ../Core/Inc/F_Maths.h ../Core/Inc/tim.h
 
@@ -195,11 +195,11 @@ Core/Src/F_Odemetry.o: ../Core/Src/F_Odemetry.c ../Core/Inc/F_Odometry.h \
 
 ../Core/Inc/gpio.h:
 
-/home/guillaume/STM32CubeIDE/workspace_1.1.0/CDFR_2020/Drivers/API_VL53L1_ULD/core/VL53L1X_api.h:
+/home/mario/Documents/ARM_DEV/CDFR_2020/Drivers/API_VL53L1_ULD/core/VL53L1X_api.h:
 
-/home/guillaume/STM32CubeIDE/workspace_1.1.0/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_platform.h:
+/home/mario/Documents/ARM_DEV/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_platform.h:
 
-/home/guillaume/STM32CubeIDE/workspace_1.1.0/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_types.h:
+/home/mario/Documents/ARM_DEV/CDFR_2020/Drivers/API_VL53L1_ULD/platform/vl53l1_types.h:
 
 ../Core/Inc/F_VL53L1X.h:
 
